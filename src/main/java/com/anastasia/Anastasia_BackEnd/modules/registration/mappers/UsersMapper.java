cbbe74a0dbcab3293bdb8214efcf4d13ee35c65b
Nahom Aron <d.nahom.selasie@gmail.com>
2026-03-13 23:08:59 -0400

    41: disable builder mapping for users mapper

--- src/main/java/com/anastasia/Anastasia_BackEnd/modules/registration/mappers/UsersMapper.java
+++ src/main/java/com/anastasia/Anastasia_BackEnd/modules/registration/mappers/UsersMapper.java
@@ -2,6 +2,7 @@ package com.anastasia.Anastasia_BackEnd.modules.registration.mappers;
 
 import com.anastasia.Anastasia_BackEnd.modules.users.model.UserDTO;
 import com.anastasia.Anastasia_BackEnd.modules.users.model.UserEntity;
+import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
@@ -9,7 +10,11 @@ import org.mapstruct.ReportingPolicy;
  Since we don't want to expose the entity to any external transactions through controller, we use mapper
  to expose only the DTO
  */
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(
+        componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        builder = @Builder(disableBuilder = true)
+)
 public interface UsersMapper {
 
     UserDTO userEntityToUserDTO(UserEntity userEntity);
