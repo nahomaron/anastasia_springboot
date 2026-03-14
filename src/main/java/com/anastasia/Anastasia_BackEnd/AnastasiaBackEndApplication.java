ed2e2e43189df8fd86dc54a7f86c240ba5159d23
Nahom Aron <d.nahom.selasie@gmail.com>
2026-03-14 02:15:13 -0400

    53: disable Redis repository auto-configuration for cache-only setup

--- src/main/java/com/anastasia/Anastasia_BackEnd/AnastasiaBackEndApplication.java
+++ src/main/java/com/anastasia/Anastasia_BackEnd/AnastasiaBackEndApplication.java
@@ -3,6 +3,7 @@ package com.anastasia.Anastasia_BackEnd;
 import com.anastasia.Anastasia_BackEnd.modules.ModulesBootstrapConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Import;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(
+		exclude = RedisRepositoriesAutoConfiguration.class,
 		scanBasePackages = {
 				"com.anastasia.Anastasia_BackEnd.core",
 				"com.anastasia.Anastasia_BackEnd.common",
